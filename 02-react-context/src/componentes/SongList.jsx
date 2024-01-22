31ea68a2525e2e35c5f909e34978f405d46426ed
Mar Bautista <mar_piscis@live.com.mx>
2024-01-21 18:08:09 -0600

    Se crea nueva carpeta dentro del proyecto

---
+++ 02-react-context/src/componentes/SongList.jsx
@@ -0,0 +1,13 @@
+import { useState,useEffect } from "react";
+import canciones from '../assets/listaCanciones.json'
+//Se utiliza el hook de estado para poder manipular los estados que se van a utiliz
+
+import React from "react";
+
+const SongList = () =>{
+    return (
+        <div className="song-list"></div>
+    )
+}
+
+export default SongList
