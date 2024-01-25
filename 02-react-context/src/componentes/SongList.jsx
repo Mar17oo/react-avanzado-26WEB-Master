e2cb1aa954aec6673105e80a1eadecc96019eb8a
Mar Bautista <mar_piscis@live.com.mx>
2024-01-25 00:21:03 -0600

    Se crean constantes para cargar listas de canciones

--- 02-react-context/src/componentes/SongList.jsx
+++ 02-react-context/src/componentes/SongList.jsx
@@ -1,12 +1,31 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import canciones from '../assets/listaCanciones.json'
 //Se utiliza el hook de estado para poder manipular los estados que se van a utiliz
 
 import React from "react";
 
 const SongList = () =>{
+    //Se crean dos estados, uno para la lista completa y otro para mostrar
+const [list, setlist] = useState([])
+const [loading, setloading] = useState(true)
+    useEffect(() => {
+        setTimeout(() =>{
+            
+        }, 2000)
+    
+    }, [third])
+    
     return (
-        <div className="song-list"></div>
+        <div className="row-container">
+            <div>
+                <h4>
+
+                </h4>
+                <p>
+
+                </p>
+            </div>
+        </div>
     )
 }
 
